3f49c0df939e249fc8dd4ddf588072c1fff63aba
mahbub14 <mahbubhasan14@gmail.com>
2024-01-12 15:11:34 +0600

    SQL

---
+++ PHP/18.Database/sql.php
@@ -0,0 +1,15 @@
+<?php
+/*
+SELECT * FROM `student`;
+SELECT * FROM `student` WHERE roll=3;
+SELECT * FROM `student` WHERE name="Mehedi";
+
+SELECT Roll , Name FROM `student` ;
+SELECT Age,Name,Roll FROM `student`;
+SELECT * FROM `student` WHERE 1 ORDER BY Age;
+SELECT * FROM `student` WHERE 1 ORDER BY DESC;
+INSERT INTO `student`(`Roll`, `Name`, `Age`) VALUES ('[value-1]','[value-2]','[value-3]')
+UPDATE `student` SET `Name`='Jakaria' WHERE Roll=4;
+DELETE FROM `student` WHERE 0
+*/
+?>
