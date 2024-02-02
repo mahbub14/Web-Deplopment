4b337c94cf7f4991530380abb552b20e4a272823
mahbub14 <mahbubhasan14@gmail.com>
2024-02-02 20:33:47 +0600

    mail

--- PHP/16.Mailing/m1.php
+++ PHP/16.Mailing/m1.php
@@ -1,9 +1,10 @@
 <?php
-$to="mahbubhasan14@gmail.com";
-$from="mahbubhasan14@gmail.com";
-$subject="How is life";
-$body="Hello\n How are you?";
-$header="From {$from}\r\n";
-mail($to,$subject,$body,$header);
-
-?>
+$to = "email_address";
+$from = "John Doe <test@example.com>";
+$subject = "How is life - HTML";
+$body = "<strong>Hello</strong><br/>কি খবর কেমন আছ?<br/>";
+$body .= "<img src='https://images.unsplash.com/photo-1553389614-1eae86aa6c7e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60' >";
+$headers = "MIME-Version: 1.0 \r\n";
+$headers .= "Content-type:text/html;charset=UTF-8\r\n";
+$headers .= "From: {$from}\r\n";
+echo mail($to, $subject, $body, $headers);
